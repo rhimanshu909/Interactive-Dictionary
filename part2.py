d43f21de5b62b73daa5973ea86f83340169c5b0e
rhimanshu <rhimanshu909@github.com>
2018-01-13 23:03:04 -0800

    Part2
    Consider an error case.
    Enter the word: macm
    Traceback (most recent call last):
      File "part1.py", line 8, in <module>
        print(translate(input_word))
      File "part1.py", line 5, in translate
        return data[word]
    KeyError: 'macm'
    
    This is the error related to the words not present in the dictionary. So, including that concern in part2.

---
+++ part2.py
@@ -0,0 +1,11 @@
+import json
+data = json.load(open("data.json"))
+
+def translate(word):
+    if word in data:
+        return data[word]
+    else:
+        return "Check the word again. It doesn't exist."
+
+input_word = input("Enter the word: ")
+print(translate(input_word))
